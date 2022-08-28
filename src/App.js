704e56901d02d17ae149859fea4bb7cfa30f0191
ValentinoMartz <valentinomartinez55@gmail.com>
2022-08-28 11:56:18 -0300

    to the navbar...

--- src/App.js
+++ src/App.js
@@ -1,5 +1,23 @@
+import Rightbar from "./components/Rightbar";
+import Sidebar from "./components/Sidebar";
+import Feed from "./components/Feed";
+import { Box, Container, Stack } from "@mui/material";
+/* import Navbar from "./components/Navbar"; */
+
 function App() {
-  return <div>holaaa!!</div>;
+  return (
+    <Box>
+      {/* <Navbar/> */}
+      <Stack direction="row" spacing={2} justifyContent="space-between">
+        <Sidebar />
+        <Feed />
+        <Rightbar />
+      </Stack>
+    </Box>
+  );
 }
 
 export default App;
+
+//----------VIDEO 26:30 Mui App Bar (Navbar)
+/* https://www.youtube.com/watch?v=fzxEECHnsvU&list=PL60WseD7DM2DHWe7W4HncUaQUVKD9nQPn&index=9&t=119s&ab_channel=LamaDev */
---
+++ src/theme.js
@@ -0,0 +1,17 @@
+import { createTheme } from "@mui/material";
+
+export const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#1760a5",
+      light: "skyblue",
+    },
+    secondary: {
+      main: "#15c630",
+      light: "skyblue",
+    },
+    otherColor: {
+      main: "#999",
+    },
+  },
+});
