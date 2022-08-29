3310fa301987ab53aaadf6256d4ce0d824b98bba
ValentinoMartz <valentinomartinez55@gmail.com>
2022-08-29 18:14:10 -0300

    navbar done

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import Rightbar from "./components/Rightbar";
 import Sidebar from "./components/Sidebar";
 import Feed from "./components/Feed";
-import { Box, Container, Stack } from "@mui/material";
+import Navbar from "./components/Navbar";
+import { Box, Stack } from "@mui/material";
 /* import Navbar from "./components/Navbar"; */
 
 function App() {
   return (
     <Box>
-      {/* <Navbar/> */}
+      <Navbar />
       <Stack direction="row" spacing={2} justifyContent="space-between">
         <Sidebar />
         <Feed />
