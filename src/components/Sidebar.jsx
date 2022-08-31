fa3ac4d8e41825e9a32db3052e0063012c62ed5c
ValentinoMartz <valentinomartinez55@gmail.com>
2022-08-31 12:18:31 -0300

    Dark Mode

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -19,7 +19,7 @@ import {
 } from "@mui/material";
 import React from "react";
 
-const Sidebar = () => {
+const Sidebar = ({ mode, setMode }) => {
   return (
     <Box flex={1} p={2} sx={{ display: { xs: "none", sm: "block" } }}>
       <Box position="fixed">
@@ -85,7 +85,9 @@ const Sidebar = () => {
               <ListItemIcon>
                 <ModeNight />
               </ListItemIcon>
-              <Switch />
+              <Switch
+                onChange={(e) => setMode(mode === "light" ? "dark" : "light")}
+              />
             </ListItemButton>
           </ListItem>
         </List>
